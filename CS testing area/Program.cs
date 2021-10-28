42b66b2912c533db770f739dbb5746cd3f4b8706
JamesRailton03 <jamesrailton23@gmail.com>
2021-10-28 07:32:04 +0100

    Add project files.

---
+++ CS testing area/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace CS_testing_area
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			
+			users new_user = new users("james",1910,true);
+
+	
+			Console.WriteLine(new_user.Username);
+			
+
+			Console.WriteLine(new_user.UserID);
+
+			
+			Console.WriteLine(new_user.User_clearance);
+			
+			
+
+
+			
+
+
+
+		}
+	}
+}
---
+++ CS testing area/users.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CS_testing_area
+{
+	internal class users
+	{
+		private string user_name;
+		private int user_id;
+		private bool user_is_cleared;
+	
+
+		public users(string name, int id, bool is_true)
+		{
+			Username = name;
+			UserID = id;
+			User_clearance = is_true;
+		}
+
+		public string Username
+		{
+			get
+			{
+				return user_name;
+			}
+			set
+			{
+				if (value != "gary")
+				{
+					user_name = value;
+				}
+				else
+				{
+					user_name = "no";
+				}
+			}
+		}
+		public int UserID
+		{
+			get
+			{
+				return user_id;
+			}
+			set
+			{
+				if (value < 1000)
+				{
+					user_id = value;
+				}
+				else
+				{
+					user_id = 0;
+				}
+			}
+		}
+		
+		// every thing above here works, all below doesnt work 
+		
+		
+		/*
+		public bool User_clearance
+		{
+			get
+			{
+				return user_is_cleared;
+			}
+			set
+			{
+				if (Username == "gary")
+				{
+					user_is_cleared = true;
+				}
+				else
+				{
+					user_is_cleared = false;
+				}
+			}
+		}
+		*/
+	}
+}
